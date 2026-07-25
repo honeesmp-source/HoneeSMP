797ee42681806b150bd1708d2d85aab2ab1051db
honeesmp-source <honeesmp@gmail.com>
2026-07-24 20:49:45 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
       {
         name: "Custom Status", // required by Discord API, not shown in the client
         state: "stalking",     // this is what people actually see
-        type: 4,               // Custom
+        type: 3,               // Custom
       },
     ],
   },
@@ -93,7 +93,7 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699",
+      primary: "#fff89e",
       secondary: "#2F3136",
 
       // Standard status colors for success/error/warning/info messages.
